64040b8f03528cae665442f864fc5a60ad0a7700
Justin Poehnelt <jpoehnelt@google.com>
2020-03-05 11:34:48 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# @googlemaps/google-maps-services-js demo
+
+1. `npm i`
+2. `npm start`
+
+> 2267.643310546875
---
+++ index.js
@@ -0,0 +1,18 @@
+const maps = require("@googlemaps/google-maps-services-node");
+
+const client = new maps.Client({});
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 45, lng: -110 }],
+      key: process.env.GOOGLE_MAPS_API_KEY
+    },
+    timeout: 1000 // milliseconds
+  })
+  .then(r => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch(e => {
+    console.log(e);
+  });
